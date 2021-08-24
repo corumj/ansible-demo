a75ec701a168442d8ef7879b2c2962b229124b1f
Jerry Corum <corum@me.com>
2021-08-24 14:38:19 -0700

    force an upgrade

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd "$( dirname "${BASH_SOURCE[0]}" )"
-ansible-galaxy collection install -r collections/requirements.yml
+ansible-galaxy collection install -r collections/requirements.yml --upgrade
 ansible-playbook aws-tower-setup.yml 
 popd
