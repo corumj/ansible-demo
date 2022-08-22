c2abc4848e8352e12026150ac23fd0c4885e2c1b
Jerry Corum <corum@me.com>
2022-08-22 09:40:57 -0700

    update to add mesh node

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 pushd "$( dirname "${BASH_SOURCE[0]}" )"
 ansible-galaxy collection install -r collections/requirements.yml --force
-ansible-playbook aws-controller-setup.yml -e @extra_vars.yml
+ansible-playbook -i aws_ec2.yml aws-controller-setup.yml -e @extra_vars.yml
 popd
